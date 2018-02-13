9c9a7ce38fb54aae1fe514f154d172dfa6d6005f
DEBRIS APRON <debrisapron@gmail.com>
2018-02-12 16:45:41 -0800

    Some more styling

--- src/components/pages/dialogue/StatementCreator.js
+++ src/components/pages/dialogue/StatementCreator.js
@@ -2,15 +2,19 @@ import h from 'react-hyperscript'
 
 function StatementCreator({ draftStatement, onChangeDraftStatement, onSubmitStatement }) {
   return (
-    h('div', [
-      h('input', { type: 'text', value: draftStatement, onChange: onChangeText }),
-      h('button', { onClick: onSubmitStatement })
+    h('form', [
+      h('label', { htmlFor: 'hm-dialogue-statement-input' }, [
+        'It\'s your turn to speak.'
+      ]),
+      h('textarea.u-full-width', {
+        id: 'hm-dialogue-statement-input',
+        placeholder: 'Compose your statement here',
+        onChange: (e) => onChangeDraftStatement({ text: e.target.value }),
+        value: draftStatement
+      }),
+      h('button', { onClick: onSubmitStatement }, 'Submit Statement')
     ])
   )
-
-  function onChangeText(e) {
-    onChangeDraftStatement({ text: e.target.value })
-  }
 }
 
 export default StatementCreator
